89e2598b725b02318a996f2c48ef8fcaf33b664b
KieuTanTai <kieutantai2x@gmail.com>
2026-06-05 15:36:02 +0700

    remove unused i_chat_request.py

--- backend/apps/core/interfaces/request/i_chat_request.py
+++ backend/apps/core/interfaces/request/i_chat_request.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from backend.apps.core.interfaces.system.i_provider import IProvider
 
 @dataclass
-class ChatRequest:
+class IChatRequest:
     conversation_id: str
     provider: IProvider
     messages: str
